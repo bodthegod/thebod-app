390199eeaa8884987d2193c2dcde654c1bd2410d
bodthegod <77025894+bodthegod@users.noreply.github.com>
2023-02-27 19:55:50 +0000

    Setup react-bootstrap and test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import Button from "react-bootstrap/Button";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <div> Deployment Test </div>
+      <Button variant="success">Success</Button>
     </div>
   );
 }

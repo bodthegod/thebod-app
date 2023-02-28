0e671e37179946c8e96fc1203a9c26b02fd96512
bodthegod <77025894+bodthegod@users.noreply.github.com>
2023-02-28 14:33:20 +0000

    Create navbar css module

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,23 +2,24 @@ import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
+import styles from '../styles/NavBar.module.css'
 
 const NavBar = () => {
   return (
-    <Navbar expand="md" fixed="top">
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
-        <Navbar.Brand>thebod</Navbar.Brand>
+        <Navbar.Brand><span><strong>thebod</strong></span></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
             <Nav.Link>
-              <i class="fa-solid fa-house-chimney-window"></i>Home
+              <i class="fa-solid fa-house-chimney-window"></i><span>Home</span>
             </Nav.Link>
             <Nav.Link>
-              <i class="fa-solid fa-right-to-bracket"></i>Log In
+              <i class="fa-solid fa-right-to-bracket"></i><span>Log In</span>
             </Nav.Link>
             <Nav.Link>
-              <i class="fa-solid fa-person-circle-plus"></i>Sign Up
+              <i class="fa-solid fa-person-circle-plus"></i><span>Sign Up</span>
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>

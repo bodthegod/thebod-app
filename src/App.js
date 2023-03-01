134db12123e4f9739c7358ae5b7d58a4ab8fc48a
bodthegod <77025894+bodthegod@users.noreply.github.com>
2023-03-01 14:21:10 +0000

    Add axiosdefaults for api functionality

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container";
 import { Route, Switch } from "react-router-dom";
 import HomePage from "./components/HomePage";
+import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 
 function App() {

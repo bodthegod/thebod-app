5e7cc3bab6678ff2cf070940b20a8b8e0b0d92e3
bodthegod <77025894+bodthegod@users.noreply.github.com>
2023-03-14 19:01:41 +0000

    Add: GeneralPostsPage with routes of liked & feed

--- src/App.js
+++ src/App.js
@@ -8,18 +8,48 @@ import SignUpForm from "./pages/auth/SignUpForm";
 import LogInForm from "./pages/auth/LogInForm";
 import PostCreateForm from "./pages/posts/PostCreateForm";
 import PostPage from "./pages/posts/PostPage";
+import GeneralPostsPage from "./pages/posts/GeneralPostsPage";
+import { useCurrentUser } from "./contexts/CurrentUserContext";
 
 function App() {
+  const currentUser = useCurrentUser();
+  const profile_id = currentUser?.profile_id || "";
   return (
     <div className={`${styles.App} psychic`}>
       <NavBar />
       <Container className={styles.Main}>
         <Switch>
-          <Route exact path="/" render={() => <HomePage />} />
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <GeneralPostsPage message="No results found, try another keyword?" />
+            )}
+          />
+          <Route
+            exact
+            path="/feed"
+            render={() => (
+              <GeneralPostsPage
+                message="No results found, try another keyword or follow a blogger."
+                filter={`owner__followed__owner__profile=${profile_id}&`}
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/liked"
+            render={() => (
+              <GeneralPostsPage
+                message="No results found, try another keyword or like a post."
+                filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`}
+              />
+            )}
+          />
           <Route exact path="/login" render={() => <LogInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/posts/create" render={() => <PostCreateForm />} />
-          <Route exact path="/posts/:id" render={() => <PostPage/>} />
+          <Route exact path="/posts/:id" render={() => <PostPage />} />
           <Route render={() => <p>Page Not Found!</p>} />
         </Switch>
       </Container>
---
+++ src/pages/posts/GeneralPostsPage.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import appStyles from "../../App.module.css";
+
+function GeneralPostsPage() {
+  return (
+    <Container>
+      <Row>
+        <Col className="py-2 p-0 p-lg-2" lg={3}>
+          <Container className={`${appStyles.Content} mb-3`}>
+            Search by tags
+          </Container>
+          <Container className={`${appStyles.Content} mb-3`}>
+            Most followed bloggers
+          </Container>
+        </Col>
+        <Col className="py-2 p-0 p-lg-2" lg={6}>
+        </Col>
+        <Col className="py-2 p-0 p-lg-2" lg={3}>
+          <Container className={`${appStyles.Content} mb-3`}>
+            Like, feed
+          </Container>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default GeneralPostsPage;

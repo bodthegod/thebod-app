958164611f3867ad1743d7ecb842d9da9f8b7538
bodthegod <77025894+bodthegod@users.noreply.github.com>
2023-03-15 19:51:25 +0000

    Add: Functions for editing and deleting posts

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Badge, Card, Media, OverlayTrigger, Tooltip } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import Avatar from "../../components/Avatar";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import styles from "../../styles/Post.module.css";
@@ -29,6 +29,20 @@ const Post = (props) => {
 
   const currentUser = useCurrentUser();
   const is_owner = currentUser?.username === owner;
+  const history = useHistory();
+
+  const handleEdit = () => {
+    history.push(`/posts/${id}/edit`);
+  };
+
+  const handleDelete = async () => {
+    try {
+      await axiosRes.delete(`/posts/${id}/`);
+      history.goBack();
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   const handleLike = async () => {
     try {
@@ -69,80 +83,85 @@ const Post = (props) => {
       timeout={{ enter: 300 }}
       classNames="fade"
     >
-    <Card className={styles.Post}>
-      <Card.Body>
-        <Media className={styles.Container}>
-          <Link to={`/profiles/${profile_id}`}>
-            <Avatar src={profile_image} height={55} />
-            {owner}
-          </Link>
-          <div className={styles.AvatarPos}>
-            <span className={`${styles.UpdatedAt} ${styles.GentleShake}`}>
-              {updated_at}
-            </span>
-            <div className={styles.EditBtn}>
-              {is_owner && postPage && <DropdownMenu />}
+      <Card className={styles.Post}>
+        <Card.Body>
+          <Media className={styles.Container}>
+            <Link to={`/profiles/${profile_id}`}>
+              <Avatar src={profile_image} height={55} />
+              {owner}
+            </Link>
+            <div className={styles.AvatarPos}>
+              <span className={`${styles.UpdatedAt} ${styles.GentleShake}`}>
+                {updated_at}
+              </span>
+              <div className={styles.EditBtn}>
+                {is_owner && postPage && (
+                  <DropdownMenu
+                    handleEdit={handleEdit}
+                    handleDelete={handleDelete}
+                  />
+                )}
+              </div>
             </div>
-          </div>
-        </Media>
-      </Card.Body>
-      <Link to={`/posts/${id}`}>
-        <Card.Img src={image} alt={title} />
-      </Link>
-      <Card.Body>
-        {title && <Card.Title className="text-center">{title}</Card.Title>}
-        {content && <Card.Text>{content}</Card.Text>}
-        {tags && (
-          <Card.Text>
-            Type:
-            <Badge variant="secondary" className={styles.PostBadge}>
-              {" "}
-              {tags}
-            </Badge>
-          </Card.Text>
-        )}
-        <div>
-          {is_owner ? (
-            <OverlayTrigger
-              placement="top"
-              overlay={<Tooltip>You can't heart this!</Tooltip>}
-            >
-              <i className={styles.Icon}>
-                <RiHeartsLine />
-              </i>
-            </OverlayTrigger>
-          ) : like_id ? (
-            <span onClick={handleUnlike}>
-              <i className={styles.LikedIcon}>
-                <RiHeartsFill />
-              </i>
-            </span>
-          ) : currentUser ? (
-            <span onClick={handleLike}>
-              <i className={styles.Icon}>
-                <RiHeartsLine />
-              </i>
-            </span>
-          ) : (
-            <OverlayTrigger
-              placement="top"
-              overlay={<Tooltip>Log in to heart this post!</Tooltip>}
-            >
-              <i className={styles.Icon}>
-                <RiHeartsLine />
-              </i>
-            </OverlayTrigger>
+          </Media>
+        </Card.Body>
+        <Link to={`/posts/${id}`}>
+          <Card.Img src={image} alt={title} />
+        </Link>
+        <Card.Body>
+          {title && <Card.Title className="text-center">{title}</Card.Title>}
+          {content && <Card.Text>{content}</Card.Text>}
+          {tags && (
+            <Card.Text>
+              Type:
+              <Badge variant="secondary" className={styles.PostBadge}>
+                {" "}
+                {tags}
+              </Badge>
+            </Card.Text>
           )}
-          {likes_total}
-          <Link to={`/posts/${id}`}>
-            <i className={`${styles.Icon}`}>
-              <RiChat3Line />
-            </i>
-          </Link>
-          {comments_total}
-        </div>
-      </Card.Body>
-    </Card>
+          <div>
+            {is_owner ? (
+              <OverlayTrigger
+                placement="top"
+                overlay={<Tooltip>You can't heart this!</Tooltip>}
+              >
+                <i className={styles.Icon}>
+                  <RiHeartsLine />
+                </i>
+              </OverlayTrigger>
+            ) : like_id ? (
+              <span onClick={handleUnlike}>
+                <i className={styles.LikedIcon}>
+                  <RiHeartsFill />
+                </i>
+              </span>
+            ) : currentUser ? (
+              <span onClick={handleLike}>
+                <i className={styles.Icon}>
+                  <RiHeartsLine />
+                </i>
+              </span>
+            ) : (
+              <OverlayTrigger
+                placement="top"
+                overlay={<Tooltip>Log in to heart this post!</Tooltip>}
+              >
+                <i className={styles.Icon}>
+                  <RiHeartsLine />
+                </i>
+              </OverlayTrigger>
+            )}
+            {likes_total}
+            <Link to={`/posts/${id}`}>
+              <i className={`${styles.Icon}`}>
+                <RiChat3Line />
+              </i>
+            </Link>
+            {comments_total}
+          </div>
+        </Card.Body>
+      </Card>
     </CSSTransition>
   );
 };

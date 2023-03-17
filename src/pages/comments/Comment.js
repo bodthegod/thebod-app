de835dcf2f785041004d7e15a2399a6353e47fe0
bodthegod <77025894+bodthegod@users.noreply.github.com>
2023-03-17 10:24:33 +0000

    Create: Dropdown menu for comment editing

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -3,9 +3,14 @@ import styles from "../../styles/Comment.module.css";
 import Avatar from "../../components/Avatar";
 import { Media } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { DropdownMenu } from "../../components/DropdownMenu";
 
 const Comment = (props) => {
   const { profile_id, profile_image, owner, updated_at, comment_info } = props;
+
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
 
   return (
     <div>
@@ -16,9 +21,12 @@ const Comment = (props) => {
         <Media.Body className="align-self-center mb-4">
           <div className={styles.CommentSection}>
             <span className={styles.CommentUsername}>{owner}</span>
-            <span className={styles.Date}> ~ {updated_at}</span>
-            <p>{comment_info}</p>
+            <span className={styles.Date}> ~ {updated_at} </span>
+            <span className={styles.DropdownMenu}>{is_owner && (
+              <DropdownMenu handleEdit={() => {}} handleDelete={() => {}} />
+            )}</span> 
           </div>
+            <p>{comment_info}</p>
         </Media.Body>
       </Media>
     </div>

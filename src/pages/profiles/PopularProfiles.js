275b73a2883645827268e171a89dcfcba2e9c62e
bodthegod <77025894+bodthegod@users.noreply.github.com>
2023-03-19 12:47:26 +0000

    Create: Profile data context for follow/unfollow

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -1,44 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Container } from "react-bootstrap";
-import { axiosReq } from "../../api/axiosDefaults";
-import appStyles from "../../App.module.css";
 import styles from "../../styles/PopularProfiles.module.css";
 import Asset from "../../components/Asset";
-import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import Profile from "./Profile";
 import { BiCrown } from "react-icons/bi";
+import { useProfileData } from "../../contexts/ProfileDataContext";
 
 const PopularProfiles = () => {
-  const [profileData, setProfileData] = useState({
-    pageProfile: { results: [] },
-    popularProfiles: { results: [] },
-  });
-  const { popularProfiles } = profileData;
-  const currentUser = useCurrentUser();
+  const { popularProfiles } = useProfileData();
 
-  useEffect(() => {
-    const handleMount = async () => {
-      try {
-        const { data } = await axiosReq.get(
-          "/profiles/?ordering=-followers_count"
-        );
-        setProfileData((prevState) => ({
-          ...prevState,
-          popularProfiles: data,
-        }));
-      } catch (err) {
-        console.log(err);
-      }
-    };
-
-    handleMount();
-  }, [currentUser]);
   return (
     <Container>
       {popularProfiles.results.length ? (
         <>
           <p className={`${styles.MostFollowedBloggers} text-center`}>
-            <BiCrown size={25} className={styles.CrownIcon}/> Most Followed
+            <BiCrown size={25} className={styles.CrownIcon} /> Most Followed
           </p>
           {popularProfiles.results.map((profile) => (
             <Profile key={profile.id} profile={profile} />

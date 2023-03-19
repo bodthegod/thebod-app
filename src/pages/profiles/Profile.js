f757d51bb7e55b242eead6ba7d7910b76642e5e9
bodthegod <77025894+bodthegod@users.noreply.github.com>
2023-03-19 12:32:37 +0000

    Add: Profile component for follow/unfollow

---
+++ src/pages/profiles/Profile.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Avatar from "../../components/Avatar";
+import styles from "../../styles/Profile.module.css";
+import btnStyles from "../../styles/CommentCreateEditForm.module.css";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
+import { AiOutlineUserAdd, AiOutlineUserDelete } from "react-icons/ai";
+
+const Profile = (props) => {
+  const { profile, imageSize = 40 } = props;
+  const { id, following_id, image, owner } = profile;
+
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
+
+  return (
+    <div className={`my-3 d-flex align-items-center flex-column`}>
+      <div>
+        <Link className="align-self-center" to={`/profiles/${id}`}>
+          <Avatar src={image} height={imageSize} />
+        </Link>
+      </div>
+      <div className={`mx-2 ${styles.ContentSplit}`}>
+        <strong>{owner}</strong>
+      </div>
+      <div className={`text-center`}>
+        {currentUser &&
+          !is_owner &&
+          (following_id ? (
+            <Button className={`${btnStyles.CommentButton}`} onClick={() => {}}>
+              unfollow <AiOutlineUserDelete size={20} />
+            </Button>
+          ) : (
+            <Button className={`${btnStyles.CommentButton}`} onClick={() => {}}>
+              follow <AiOutlineUserAdd size={20} />
+            </Button>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default Profile;

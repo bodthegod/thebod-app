40b8c113a32a34f947f3a312280191ebcb1ffc18
bodthegod <77025894+bodthegod@users.noreply.github.com>
2023-03-22 12:43:03 +0000

    Add: Active page toolbar colour for visual cues

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import styles from "../styles/Toolbar.module.css";
 import appStyles from "../App.module.css";
@@ -8,9 +8,10 @@ import { RiHeartsFill } from "react-icons/ri";
 import ScrollToTopButton from "./ScrollToTopButton";
 
 const Toolbar = () => {
+  const { pathname } = useLocation();
   return (
-    <Container className={`${appStyles.Content} mb-2`}>
-      <Link to="/feed">
+    <Container className={`${appStyles.Content} ${styles.ActivePath} mb-2`}>
+      <Link to="/feed" className={pathname === "/feed" ? styles.active : null}>
         <p>
           <i className={`${styles.Icon} ${styles.MyFeedIcon} m-3`}>
             <MdOutlineSportsHandball />
@@ -18,17 +19,23 @@ const Toolbar = () => {
           <strong>My Feed</strong>
         </p>
       </Link>
-      <Link to="/liked">
+      <Link
+        to="/liked"
+        className={pathname === "/liked" ? styles.active : null}
+      >
         <p>
-          <i className={`${styles.Icon} ${styles.HeartIcon} m-3`}>
+          <i className={`${styles.Icon} ${styles.HeartIcon} ${styles.ActivePath} m-3`}>
             <RiHeartsFill />
           </i>
           <strong>My Hearts </strong>
         </p>
       </Link>
-      <Link to="/posts/create">
+      <Link
+        to="/posts/create"
+        className={pathname === "/posts/create" ? styles.active : null}
+      >
         <p>
-          <i className={`${styles.Icon} ${styles.CreatePostIcon} m-3`}>
+          <i className={`${styles.Icon} ${styles.CreatePostIcon} ${styles.ActivePath} m-3`}>
             <MdOutlineLibraryAdd />
           </i>
           <strong>Create Post</strong>

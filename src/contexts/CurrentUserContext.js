da415d5d35e28a4c4704bd8e87d76afd28526bfe
bodthegod <77025894+bodthegod@users.noreply.github.com>
2023-03-25 15:13:54 +0000

    Fix: Empty block statement

--- src/contexts/CurrentUserContext.js
+++ src/contexts/CurrentUserContext.js
@@ -20,6 +20,7 @@ export const CurrentUserProvider = ({ children }) => {
       const { data } = await axiosRes.get("dj-rest-auth/user/");
       setCurrentUser(data);
     } catch (err) {
+      return err
     }
   };
 
